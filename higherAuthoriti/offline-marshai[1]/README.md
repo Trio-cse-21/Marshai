a7b66cf9338c03419df07cf4513d37086677f0e2
Trio <166159404+Trio-cse-21@users.noreply.github.com>
2024-05-31 11:10:43 +0530

    Add files via upload

---
+++ higherAuthoriti/offline-marshai[1]/README.md
@@ -0,0 +1,1 @@
+# MARSHAI-UMS
---
+++ higherAuthoriti/offline-marshai[1]/timeTable.js
@@ -0,0 +1,134 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'DBMS130',
+        type: 'Meeting '
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'MTH166',
+        type: 'Meeting Schedule'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Meeting'
+    
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '27-304Y',
+        subject: 'MTH166',
+        type: 'Meeting'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Meeting'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Meeting'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Meeting'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'DBMS130',
+        type: 'Meeting'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'CS200',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'CS849',
+        type: 'noMeeting'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Meeting'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: ''
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'CS849',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Lecture'
+    }
+]
